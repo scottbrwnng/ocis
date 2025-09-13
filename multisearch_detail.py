e7f8b0d76de4788e27ac31ad7a97df57e469c6bb
scottbrwnng <72707817+scottbrwnng@users.noreply.github.com>
2025-09-13 19:30:51 -0400

    removed user agent param from headers

--- multisearch_detail.py
+++ multisearch_detail.py
@@ -20,7 +20,6 @@ class Searcher:
     def create_session(self):
         headers = {
             "Accept": "application/json",
-            # "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Safari/605.1.15", #TODO: figure out what this is
             "Content-Type": "application/json;charset=UTF-8",
             'Cookie': f'OES_TC_JSESSIONID={self.cookie}'
         }
