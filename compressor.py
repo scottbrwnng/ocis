c0eb78cdd89760c100596650351467ff208385e4
scottbrwnng <72707817+scottbrwnng@users.noreply.github.com>
2025-10-08 20:49:08 -0400

    modified compression to use size of case detail table in file name

--- compressor.py
+++ compressor.py
@@ -1,12 +1,28 @@
 import os
 import json
 import gzip
+import duckdb as ddb
+
+# 1. scrape
+# 2. compress
+# 3. load CASE_DTL_STGN
+# 4. delete dtl folder
+# 5. create dtl folder
+# 6. repeat
+
+def table_size() -> int:
+    with ddb.connect('ocis') as conn:
+        size = conn.execute('SELECT COUNT(*) FROM CASE_DTL_STGN').fetchone()[0]
+        print(size)
+    return size
 
 
 def comprx():
     all_files = os.listdir('./dtl/')
+    size = table_size()
     out = []
     for i, x in enumerate(all_files, start=1):  # start at 1
+        i += size
         with open(f'./dtl/{x}', 'rb') as f: 
             try:
                 j = json.loads(f.read())
