01cc2c272b17678bb5ea7ffed007304555f98481
scottbrwnng <72707817+scottbrwnng@users.noreply.github.com>
2025-10-26 00:00:29 -0400

    fixed broken case hearings search

--- search.py
+++ search.py
@@ -2,60 +2,91 @@ import requests
 import json
 from datetime import date, timedelta, datetime
 import warnings
+import random
 import uuid
+import time
 import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
+import logging
+
+hs = logging.StreamHandler()
+hf = logging.FileHandler('logs.log')
+logging.basicConfig(
+    level=logging.INFO, handlers=[hs, hf], 
+    format='xxx -- %(asctime)s [%(levelname)s] [%(threadName)s] %(message)s', 
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+log = logging.getLogger(__name__)
 
 warnings.filterwarnings('ignore')
 
-def create_session():
-    url = "https://eapps.courts.state.va.us/ocis-rest/api/public/termsAndCondAccepted"
-    res = requests.get(url, verify=False)
-    cook = res.cookies['OES_TC_JSESSIONID']
 
-    headers = {
-        "Accept": "application/json, text/plain, */*",
-        "Content-Type": "application/json;charset=UTF-8",
-        'Cookie': f'OES_TC_JSESSIONID={cook}'
-    }
-    s = requests.Session()
-    s.headers=headers
-    return s
-    
-def date_range(start_date:date, end_date:date) -> list:
-    diff = (end_date-start_date).days
-    old = {datetime.strptime(x[:10], '%Y-%m-%d').date() for x in os.listdir('./dumps') if '.json' in x}
-    d = []
-    for x in range(diff):
-        new = start_date + timedelta(days=x)
-        if new not in old:
-            d.append(start_date + timedelta(days=x))
-    return d
+
+
 
 
 class Searcher:
-    def __init__(self, start_date:date) -> None:
-        self.session = create_session()
-        self._date = start_date
+    def __init__(self) -> None:
+        self.pay = None
+        self.session = self.create_session()
+        self.proxy_list = load_proxies()
+        self.proxy = random.choice(self.proxy_list)
         self.idx = None
-        self.out = []
 
-    def extract(self) -> dict:
-        _date = self._date.strftime('%m/%d/%Y')
-        payload = {"courtLevels":[],"divisions":["Adult Criminal/Traffic"],"selectedCourts":[],"searchString":[_date],"searchBy":"HD"}
+
+    def create_session(self):
+
+        def get_cookie() -> str:
+            while True:
+                try:
+                    url = "https://eapps.courts.state.va.us/ocis-rest/api/public/termsAndCondAccepted"
+                    res = requests.get(url, verify=False)
+                    cookie = res.cookies['OES_TC_JSESSIONID']
+                    break
+                except Exception as e:
+                    print(f'{self.pay} {type(e).__name__}: {e}')
+            return cookie
+
+        cookie = get_cookie()
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json;charset=UTF-8",
+            'Cookie': f'OES_TC_JSESSIONID={cookie}'
+        }
+        s = requests.Session()
+        s.headers=headers
+        return s    
+
+    def extract(self, _date:date) -> dict:
+        self._date = _date
+        self.pay = {"courtLevels":[],"divisions":["Adult Criminal/Traffic"],"selectedCourts":[],"searchString":[self._date.strftime('%m/%d/%Y')],"searchBy":"HD"}
         if self.idx: # may not need if statement
-            payload.update({"endingIndex":self.idx})
-        res = self.session.post(
-            'https://eapps.courts.state.va.us/ocis-rest/api/public/search',
-            json = payload,
-            verify=False,
-            timeout=10
-        ).json()
-        return res
-        
+            self.pay.update({"endingIndex":self.idx})
+        retries = 0 
+        while retries < 4:
+            try:
+                res = self.session.post(
+                    'https://eapps.courts.state.va.us/ocis-rest/api/public/search',
+                    json = self.pay,
+                    verify=False,
+                    proxies = {'http': self.proxy},
+                    timeout=5
+                ).json()
+                return res
+            except Exception as e:
+                print(f'{self.pay} {type(e).__name__}: {e}')
+                time.sleep(1 / random.randint(5, 10))
+                if retries > 0:
+                    time.sleep(1 / random.randint(5, 10))
+                if retries > 1:
+                    self.proxy = random.choice(self.proxy_list)
+                if retries > 2:
+                    self.session = self.create_session()
+                retries+=1
+
     def last_result_index(self, res:dict=None) -> None:
-        if res.get('context').get('entitiy').get('payload').get('lastResponseIndex'):
-            self.idx = res.get('context').get('entitiy').get('payload').get('lastResponseIndex')
+        if res.get('context').get('entity').get('payload').get('lastResponseIndex'):
+            self.idx = res.get('context').get('entity').get('payload').get('lastResponseIndex')
         else:
             self.idx = None
             
@@ -67,26 +98,44 @@ class Searcher:
             json.dump(res, f, indent=4)
 
 
+def load_proxies() -> list:
+    with open('proxies.txt', 'r') as f:
+        proxies = f.read().split('\n')
+    return proxies
+
+
+def date_range(start_date:date, end_date:date) -> list:
+    diff = (end_date-start_date).days
+    old = {datetime.strptime(x[:10], '%Y-%m-%d').date() for x in os.listdir('./dumps') if '.json' in x}
+    d = []
+    for x in range(diff):
+        new = start_date + timedelta(days=x)
+        if new not in old:
+            d.append(start_date + timedelta(days=x))
+    return d
+
+
 def run(single_date:date):
     print('running', single_date)
-    search = Searcher(single_date)
+    search = Searcher()
     while True:
-        res = search.extract()
+        res = search.extract(single_date)
+        if not res:
+            continue
         search.write_json(res)
         search.last_result_index(res)
         if not search.idx:
             break  # no more results for this date
     print('Finished', single_date)
 
+
 def main():
     start_date = date(year = 1980, month = 1, day = 1)
     end_date = date.today()
     dates = date_range(start_date, end_date)[10:]
-    # threads are fine for network I/O and avoid pickling problems
-    with ThreadPoolExecutor(max_workers=8) as executor:
-        futures = {executor.submit(run, d): d for d in dates}
-        for future in futures:
-            future.result()
+    for d in dates:
+        run(d)
+
 
 
 if __name__ == '__main__':
